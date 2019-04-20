fc41eb604f1a4e44f29bfa8c4f3b1015ae1afc8c
Aleksei Ermolov <aleksei_ermolov@epam.com>
2019-04-20 16:34:55 +0400

    master: fix template

--- user_app/usercard/src/app/components/user-list/user-list.component.ts
+++ user_app/usercard/src/app/components/user-list/user-list.component.ts
@@ -1,50 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/shared/_shared';
-import { Router } from '@angular/router';
-import { trigger, transition, query, animate, style } from '@angular/animations';
-import { RootStoreState, UsersStoreSelectors } from 'src/app/root-store';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { User } from "src/app/shared/_shared";
+import { Router } from "@angular/router";
+import {
+  trigger,
+  transition,
+  query,
+  animate,
+  style
+} from "@angular/animations";
+import { RootStoreState, UsersStoreSelectors } from "src/app/root-store";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css'],
+  selector: "app-user-list",
+  templateUrl: "./user-list.component.html",
+  styleUrls: ["./user-list.component.css"],
   animations: [
-    trigger('cardAnimation', [
-      transition('* => *', [
-        query(':enter', [
-          style({ opacity: 0 }),
-          animate('1s', style({ opacity: 1 }))
-        ], { optional: true })
+    trigger("cardAnimation", [
+      transition("* => *", [
+        query(
+          ":enter",
+          [style({ opacity: 0 }), animate("1s", style({ opacity: 1 }))],
+          { optional: true }
+        )
       ])
     ])
   ]
 })
 export class UserListComponent implements OnInit {
   users: User[];
+
   users$: Observable<User[]>;
+  count$: Observable<number>;
+
   error$: Observable<any>;
   isLoading$: Observable<boolean>;
 
   constructor(
     private router: Router,
     private store$: Store<RootStoreState.State>
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.populateUsers();
   }
 
   populateUsers() {
-    this.store$.select(UsersStoreSelectors.selectAllItems)
-      .subscribe(data => {
-        this.users = data;
-      });
+    this.users$ = this.store$.select(UsersStoreSelectors.selectAllItems);
+    this.count$ = this.store$.select(UsersStoreSelectors.selectFeatureCount);
   }
 
   onCardClick(userId: number) {
-    this.router.navigate(['/user/', userId]);
+    this.router.navigate(["/user/", userId]);
   }
-
 }
--- user_app/usercard/src/app/root-store/user-store/selectors.ts
+++ user_app/usercard/src/app/root-store/user-store/selectors.ts
@@ -1,41 +1,63 @@
 import {
-    createFeatureSelector,
-    createSelector,
-    MemoizedSelector
-} from '@ngrx/store';
+  createFeatureSelector,
+  createSelector,
+  MemoizedSelector
+} from "@ngrx/store";
 
-import { featureAdapter, State } from './state';
-import { UserModel } from 'src/app/shared/models/user';
+import { featureAdapter, State } from "./state";
+import { UserModel } from "src/app/shared/models/user";
 
 export const getError = (state: State): any => state.error;
-
 export const getIsLoading = (state: State): boolean => state.isLoading;
+export const getCount = (state: State): number => state.ids.length;
 
-export const selectState: MemoizedSelector<object, State>
-    = createFeatureSelector<State>('users');
+export const selectState: MemoizedSelector<
+  object,
+  State
+> = createFeatureSelector<State>("users");
 
-export const selectAllItems: (state: object) =>
-    UserModel[] = featureAdapter.getSelectors(selectState).selectAll;
+export const selectAllItems: (
+  state: object
+) => UserModel[] = featureAdapter.getSelectors(selectState).selectAll;
 
-export const selectById = (id: number) =>
-    createSelector(featureAdapter.getSelectors(selectState).selectAll, (all: UserModel[]) => {
-        const defaultItem = new UserModel(0, '', 0, [], '');
-        if (all && all.length > 0) {
-            return all.find(p => p.id === id) || defaultItem;
-        } else {
-            return defaultItem;
-        }
-    });
-
-export const select = () =>
-    createSelector(featureAdapter.getSelectors(selectState).selectAll, (all: UserModel[]) => {
-        return all;
-    });
-
-export const selectError: MemoizedSelector<object, any> = createSelector(
-    selectState,
-    getError
+export const selectFeatureCount: MemoizedSelector<
+  object,
+  number
+> = createSelector(
+  selectState,
+  getCount
 );
 
-export const selectIsLoading: MemoizedSelector<object, boolean> =
-    createSelector(selectState, getIsLoading);
+export const selectById = (id: number) =>
+  createSelector(
+    featureAdapter.getSelectors(selectState).selectAll,
+    (all: UserModel[]) => {
+      const defaultItem = new UserModel(0, "", 0, [], "");
+      if (all && all.length > 0) {
+        return all.find(p => p.id === id) || defaultItem;
+      } else {
+        return defaultItem;
+      }
+    }
+  );
+
+export const select = () =>
+  createSelector(
+    featureAdapter.getSelectors(selectState).selectAll,
+    (all: UserModel[]) => {
+      return all;
+    }
+  );
+
+export const selectError: MemoizedSelector<object, any> = createSelector(
+  selectState,
+  getError
+);
+
+export const selectIsLoading: MemoizedSelector<
+  object,
+  boolean
+> = createSelector(
+  selectState,
+  getIsLoading
+);
